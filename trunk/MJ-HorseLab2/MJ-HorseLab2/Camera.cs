c276b54e24cfbd6693d20b53692e2ff13b8d5dfb
magnusson1987@gmail.com <magnusson1987@gmail.com@ce2bb0dd-39dc-73d8-0340-1bd3ebb94a74>
2013-12-05 15:19:35 +0000

    KUBVOXEL

---
+++ trunk/MJ-HorseLab2/MJ-HorseLab2/Camera.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MJ_HorseLab2
+{
+    class Camera
+    {
+
+        #region Fields
+        private Vector3 position = Vector3.Zero;
+        private float rotation;
+        private Vector3 lookAt;
+        private Vector3 baseCameraReference = new Vector3(0, 0, 1);
+        private bool needViewResync = true;
+
+        private Matrix cachedViewMatrix;
+        #endregion
+
+        #region Properties
+        public Matrix projection { get; private set; }
+
+        public Vector3 Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+                UpdateLookAt();
+            }
+        }
+
+        public float Rotation
+        {
+            get { return rotation; }
+            set
+            {
+                rotation = value;
+                UpdateLookAt();
+            }
+        }
+
+        public Matrix View
+        {
+            get
+            {
+
+                if (needViewResync)
+                    cachedViewMatrix = Matrix.CreateLookAt(position, lookAt, Vector3.Up);
+
+                return cachedViewMatrix;
+            }
+        }
+        #endregion
+
+        #region Constructor
+
+        public Camera(Vector3 position, float rotation, float aspectRatio, float frontClip, float backClip)
+        {
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, frontClip, backClip);
+            MoveTo(position, rotation);
+        }
+        #endregion
+
+
+        #region Helper Methods
+
+        private void UpdateLookAt()
+        {
+            Matrix rotationMatrix = Matrix.CreateRotationY(rotation);
+            Vector3 lookAtOffset = Vector3.Transform(baseCameraReference, rotationMatrix);
+            lookAt = position + lookAtOffset;
+            needViewResync = true;
+        }
+
+        public void MoveTo(Vector3 position, float rotation)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            UpdateLookAt();
+        }
+
+        public Vector3 PreviewMove(float scale)
+        {
+            Matrix rotate = Matrix.CreateRotationY(rotation);
+            Vector3 forward = new Vector3(0, 0, scale);
+            forward = Vector3.Transform(forward, rotate);
+            return (position + forward);
+        }
+
+        public void MoveForward(float scale)
+        {
+            MoveTo(PreviewMove(scale), rotation);
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ trunk/MJ-HorseLab2/MJ-HorseLab2/Voxel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MJ_HorseLab2
+{
+    class Voxel
+    {
+        private List<VertexPositionTexture> vertices = new List<VertexPositionTexture>();
+        private Vector3 location;
+        private Texture2D texture;
+        private GraphicsDevice device;
+        private VertexBuffer voxelBuffer;
+
+        public Voxel(GraphicsDevice device, Texture2D texture)
+        {
+            this.device = device;
+            this.texture = texture;
+            SetUpVertices();
+        }
+
+        private void SetUpVertices()
+        {
+            BuildFace(new Vector3(0, 0, 0), new Vector3(0, 1, 1));
+            BuildFace(new Vector3(0, 0, 1), new Vector3(1, 1, 1));
+            BuildFace(new Vector3(1, 0, 1), new Vector3(1, 1, 0));
+            BuildFace(new Vector3(1, 0, 0), new Vector3(0, 1, 0));
+            voxelBuffer = new VertexBuffer(device, VertexPositionTexture.VertexDeclaration, vertices.Count, BufferUsage.WriteOnly);
+            voxelBuffer.SetData<VertexPositionTexture>(vertices.ToArray());
+        }
+
+        private void BuildFace(Vector3 point1, Vector3 point2)
+        {
+            vertices.Add(BuildVertex(point1.X, point1.Y, point1.Z, 1, 0));
+            vertices.Add(BuildVertex(point1.X, point2.Y, point1.Z, 1, 1));
+            vertices.Add(BuildVertex(point2.X, point2.Y, point2.Z, 0, 1));
+            vertices.Add(BuildVertex(point2.X, point2.Y, point2.Z, 0, 1));
+            vertices.Add(BuildVertex(point2.X, point1.Y, point2.Z, 0, 0));
+            vertices.Add(BuildVertex(point1.X, point1.Y, point1.Z, 1, 0));
+
+        }
+
+        private VertexPositionTexture BuildVertex(float x, float y, float z, float u, float v)
+        {
+            return new VertexPositionTexture(new Vector3(x, y, z), new Vector2(u, v));
+        }
+
+        public void PositionCube(float minDistance, Vector3 position)
+        {
+            location = new Vector3(1.5f, 0.5f, 1.5f);
+
+        }
+
+        public void Draw(Camera camera, BasicEffect effect)
+        {
+            effect.VertexColorEnabled = false;
+            effect.TextureEnabled = true;
+            effect.Texture = texture;
+            Matrix center = Matrix.CreateTranslation(new Vector3(-0.5f, -0.5f, -0.5f));
+            Matrix translate = Matrix.CreateTranslation(location);
+            effect.World = center * translate;
+            effect.View = camera.View;
+            effect.Projection = camera.projection;
+            effect.CurrentTechnique.Passes[0].Apply();
+
+            device.SetVertexBuffer(voxelBuffer);
+            device.DrawPrimitives(PrimitiveType.TriangleList, 0, voxelBuffer.VertexCount / 3);
+        }
+    }
+}
